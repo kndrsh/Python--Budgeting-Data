5f98a6de00283b7e5170caf1bb6fbcef581afab0
Isha kundra <kndrsh@gmail.com>
2021-04-08 09:44:00 -0700

    number of votes

--- PyPull/Main.py
+++ PyPull/Main.py
@@ -1,6 +1,26 @@
 # import modules to load csv data
-import os
+
 import csv
 
 with open('election_data.csv','r') as csv_file :
     csv_reader = csv.reader(csv_file,delimiter =",")
+    
+    # Declare your variables 
+    votes = []
+    county = []
+    candidates = []
+    khan = []
+    correy = []
+    li = []
+    otooley = []
+    
+    for row in csv_reader:
+        votes.append(row[0])
+        county.append(row[1])
+        candidates.append(row[2])
+        
+        # number of votes
+        total_votes = (len(votes))
+        print(total_votes)
+        
+        
