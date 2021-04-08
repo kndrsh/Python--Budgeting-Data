76950e444291cc98f37be27591ec4874dc5c4c56
Isha kundra <kndrsh@gmail.com>
2021-04-07 18:27:47 -0700

    creating folders

---
+++ PyBank/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  7 17:24:26 2021
+
+@author: kndrs
+"""
+
+
+
---
+++ PyPull/Main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  7 18:19:58 2021
+
+@author: kndrs
+"""
+
