eadf3f24dce469a6eeb509ac2c2374c2c99922c4
Isha kundra <kndrsh@gmail.com>
2021-04-08 01:09:46 -0700

    3rd commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,6 +22,37 @@ with open('budget_data.csv','r') as csv_file :
         total_months.append(row[0])
         total_profit.append(row[1])
         print(len(total_months))
+        
+        # Average Profit Change 
+        profit_average = sum(int(total_profit))/len(monthly_profit_change)
+        
+        
+        # monthly change in profits ( c will represent change)
+        # have to use for loop to iterate through the profits to get monthly change
+        
+        for c in range(len(total_profit)-1):
+            monthly_profit_change.append(int(total_profit[c+1])- int(total_profit[c]))
+            
+            #Finding Greatest Increase & Greatest Decrease (Profits)
+            # Can use the max & min function 
+            
+            max_greatest_increase = max(monthly_profit_change)
+            max_greatest_decrease = min(monthly_profit_change)
+            
+            
+            max_greatest_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
+            max_greatest_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1
+            
+            # Print Statements 
+            print("Financial Analysis")
+            print("---------------------------------")
+            print(f"Total months: {len(total_months)}")
+            print(f"Average Profit: {profit_average}")
+            print(f"Greatest Increase In Profits: {total_months[max_greatest_increase_month]} (${(str(max_greatest_increase))})") 
+            
+            
+       
+            
        
         
     
